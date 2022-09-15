20cfe02fcd1c8c2b1ff0d33cf8aa1c658ebd42e8
Patrick Moss <patrick@busnit.com>
2022-09-15 09:16:50 -0400

    Update deploy.ps1

--- servicebus-arm/deploy.ps1
+++ servicebus-arm/deploy.ps1
@@ -1,8 +1,7 @@
 #
 Connect-AzAccount
 Get-AzSubscription
- $context = Get-AzSubscription -SubscriptionId 'd002407a-704a-4a42-bcf8-ee7cb5e23d00'
-# $context = Get-AzSubscription -SubscriptionId '6a2b76bf-938a-4481-b064-5635f271fad2'
+ $context = Get-AzSubscription -SubscriptionId 'redacted'
 Set-AzContext $context
 Set-AzDefault -ResourceGroupName rg-sb-prod-ncus-02
 $templateFile = "template.json"
